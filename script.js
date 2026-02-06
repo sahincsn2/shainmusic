ebf23df447ec07e1ae9c16b1aba45b9eafbc23b1
sahincsn2 <152686091+sahincsn2@users.noreply.github.com>
2026-02-07 01:16:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("ShainMusic sitesi yüklendi!");
+let audioPlayer;
+
+document.addEventListener("DOMContentLoaded", function () {
+    audioPlayer = document.getElementById("audioPlayer");
+});
+
+function playBefore() {
+    audioPlayer.src = "audio/song-before.mp3";
+    audioPlayer.play();
+}
+
+function playAfter() {
+    audioPlayer.src = "audio/song-after.mp3";
+    audioPlayer.play();
+}
